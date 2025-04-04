ee3b0cdce7d3e9128cd9c862a50d073bbee12e73
Ro_gdre5438_bot <roguenobody247@gmail.com>
2025-04-05 09:45:19 +1100

    genrealised the code and made funcs

--- code/python/thesis/data_analysis.py
+++ code/python/thesis/data_analysis.py
@@ -2,118 +2,91 @@ import matplotlib.pyplot as plt
 import numpy as np
 #lib to read in csv files 
 import pandas as pd
+from data_extraction import extract_data
 
-def main(): 
-    #read in data from ./data/fresh-leaf-spectra-to-estimate-lma-over-neon-domains-in-eastern-united-states.csv
-    path = './data/fresh-leaf-spectra-to-estimate-lma-over-neon-domains-in-eastern-united-states.csv'
-    data = pd.read_csv(path)
-    #print(data.to_string()) 
+def count_samples_by_symbol(data, sort_term):
+   
+    Symbol_count = {}
+    for index, row in data.iterrows(): 
+        symbol = row[sort_term]
+        if symbol in Symbol_count:
+            Symbol_count[symbol] += 1
+        else:  
+            Symbol_count[symbol] = 1
+    return Symbol_count
 
-    #find index of USDA Symbol header in data
-    #print(data.columns.to_list().index('USDA Symbol')) 
-    data = data.dropna() 
+def extract_xpoints(data, data_start=11):
+    xpoints = list(data)
+    xpoints = xpoints[data_start:] 
+    return xpoints 
+
+#usage:
+#for index, row in data.iterrows():
+    #    ypoints = extract_ypoints(row)
+def extract_ypoints(data, toggle_float_conversion, data_start=11): 
+    ypoints = data.iloc[data_start:].values
+    # convert from np.float to float
+    if toggle_float_conversion is True:
+        ypoints = np.array(ypoints, dtype=float)
+    return ypoints
+
+def plot_wavelength(data, sort_term, data_start=11):
+    
+   #the x values are in the header of the data frame 
+    xpoints = extract_xpoints(data,data_start) 
 
     #for every row in data, add new USDA Symbol to a dic and count them if they aleard exist
-    USDA_Symbol = {}
-    for index, row in data.iterrows(): 
-        symbol = row['USDA Symbol']
-        if symbol in USDA_Symbol:
-            USDA_Symbol[symbol] += 1
-        else:  
-            USDA_Symbol[symbol] = 1
-    
-    #sort the USDA_Symbol dic by value
-    USDA_Symbol = dict(sorted(USDA_Symbol.items(), key=lambda item: item[1], reverse=True))
-        #print the USDA_Symbol dic
-    #print(USDA_Symbol)
 
-    #if USDA_Symbol value is > 100 add to a new dic exclude nan
-    USDA_Symbol_100 = {}
-    #make list
-    USDA_Symbol_for_analysis = []
-    for key, value in USDA_Symbol.items():
-        if value > 100:
-            USDA_Symbol_100[key] = value 
-            #store key in a list 
-            USDA_Symbol_for_analysis.append(key)
-
-    #print the USDA_Symbol_100 dic
-    #print(USDA_Symbol_100, "\n",  USDA_Symbol_for_analysis)
-
-    #extract data from the original data frame where USDA Symbol is in USDA_Symbol_for_analysis 
-    data = data[data['USDA Symbol'].isin(USDA_Symbol_for_analysis)]
-    #make a csv file of the data
-    path = './data/HS_data_for_analysis.csv'
-    data.to_csv(path, index=False)  
-    data = pd.read_csv(path)
-    #sort data by USDA Symbol
-    data = data.sort_values(by=['USDA Symbol'])
-
-   #the x values are in the header of the data frame 
-    xpoints = list(data)
-    xpoints = xpoints[11:] 
+    Symbol_sample_count = count_samples_by_symbol(data,sort_term) 
 
     # Initialize variables for plotting
     last_symbol = None
     plt.figure()
 
-    for index, row in data.iterrows():
-        symbol = row['USDA Symbol']
-        # Ensure xpoints and ypoints are 1D arrays for plotting
-        ypoints = row.iloc[11:].values
-        ypoints = np.array(ypoints, dtype=float)
+    for index, row in data.iterrows(): 
+        symbol = row[sort_term] 
+        
+        ypoints = extract_ypoints(row, True, data_start)
 
         if symbol != last_symbol and last_symbol is not None:
             # Show the current figure and start a new one
-            plt.title(f"USDA Symbol: {last_symbol}")
+            plt.title(f"{sort_term}: {last_symbol}")
             plt.xlabel('Wavelength')
             plt.ylabel('Reflectance')
             #reduce the number of ticks on the x axis
             plt.xticks(np.arange(0, len(xpoints), len(xpoints)/10), fontsize=8)
+            
+          
+            # plt.text(0.5, 0.5, f"Samples: {Symbol_sample_count[last_symbol]}", fontsize=12, ha='left', va='top') 
+              # display Symbol_sample_count(last_symbol) on the figure top left
+            plt.text(0.75, 0.75, f"Samples: {Symbol_sample_count[last_symbol]}", fontsize=12, ha='left', va='top', transform=plt.gca().transAxes)
             plt.show()
-            plt.figure()
+            plt.figure() 
             #save plot as a png file
             plt.savefig(f'./data/{last_symbol}.png')
-
+            
         # Plot the data
         plt.plot(xpoints, ypoints, label=f"Sample {index}")
         last_symbol = symbol
 
     # Show the last figure
     if last_symbol is not None:
-        plt.title(f"USDA Symbol: {last_symbol}")
+        plt.title(f"{sort_term}: {last_symbol}")
         plt.xlabel('Wavelength')
         plt.ylabel('Reflectance')
         plt.xticks(np.arange(0, len(xpoints), len(xpoints)/10), fontsize=8) 
         plt.show()
         plt.savefig(f'./data/{last_symbol}.png') 
 
+def main():
+    extract_data() 
+    path = './data/HS_data_for_analysis.csv' 
+    data = pd.read_csv(path) 
+    #sort data by USDA Symbol
+    data = data.sort_values(by=['USDA Symbol'])
+    plot_wavelength(data,'USDA Symbol')
 
 
-"""
-    
-
-    for row in data.iterrows():
-        # Ensure xpoints and ypoints are 1D arrays for plotting
-        ypoints = data.iloc[row, 11:].values 
-        #convert from np.float to float
-        ypoints = np.array(ypoints, dtype=float)
-        #print(xpoints)
-        # Plot the data
-    
-    plt.plot(xpoints, ypoints)  
-
-  
-    #print(ypoints)
-
-    
-    #reduce the number of x ticks to 10
-    plt.xticks(np.arange(0, len(xpoints), len(xpoints)/10), fontsize=8)
-
-    #set the x and y axis labels    
-    plt.xlabel('Wavelength', fontsize=10)
-    plt.ylabel('Reflectance', fontsize=10)
-    plt.show() """
 
 if __name__ == "__main__":
-    main()
+    main() 
---
+++ code/python/thesis/data_extraction.py
@@ -0,0 +1,126 @@
+import matplotlib.pyplot as plt
+import numpy as np
+#lib to read in csv files 
+import pandas as pd
+
+def extract_data(sample_token = 'USDA Symbol', 
+                path = './data/fresh-leaf-spectra-to-estimate-lma-over-neon-domains-in-eastern-united-states.csv',  
+                final_path='./data/HS_data_for_analysis.csv'):
+    
+    #read in data from path
+    data = pd.read_csv(path)
+    #print(data.to_string()) 
+
+    #remove nan values from the data frame 
+    data = data.dropna() 
+
+    #for every row in data, add new USDA Symbol to a dic and count them if they aleard exist
+    token_Symbol = {}
+    for index, row in data.iterrows(): 
+        symbol = row[sample_token]
+        if symbol in token_Symbol:
+            token_Symbol[symbol] += 1
+        else:  
+            token_Symbol[symbol] = 1
+    
+    #sort the token_Symbol dic by value
+    token_Symbol = dict(sorted(token_Symbol.items(), key=lambda item: item[1], reverse=True))
+    #print(token_Symbol)
+
+    
+    token_Symbol_100 = {}
+    token_Symbol_for_analysis = []
+
+    #if token_Symbol value is > 100 add to a new dic 
+        #ensure that the number of samples is > 100 
+    for key, value in token_Symbol.items():
+        if value > 100:
+            token_Symbol_100[key] = value 
+            #store key in a list 
+            token_Symbol_for_analysis.append(key)
+
+    #print the token_Symbol_100 dic
+    print(token_Symbol_100, "\n",  token_Symbol_for_analysis) 
+
+    #extract data from the original data frame where USDA Symbol is in token_Symbol_for_analysis 
+    data = data[data[sample_token].isin(token_Symbol_for_analysis)]
+    #make a csv file of the data
+    path = final_path
+    data.to_csv(path, index=False)  
+    return data
+
+def main(): 
+    extract_data() 
+    
+
+
+
+"""
+    path = './data/HS_data_for_analysis.csv' 
+    #data = pd.read_csv(path)
+    #sort data by USDA Symbol
+    data = data.sort_values(by=['USDA Symbol'])
+
+   #the x values are in the header of the data frame 
+    xpoints = list(data)
+    xpoints = xpoints[11:] 
+
+    # Initialize variables for plotting
+    last_symbol = None
+    plt.figure()
+
+    for index, row in data.iterrows():
+        symbol = row['USDA Symbol']
+        # Ensure xpoints and ypoints are 1D arrays for plotting
+        ypoints = row.iloc[11:].values
+        ypoints = np.array(ypoints, dtype=float)
+
+        if symbol != last_symbol and last_symbol is not None:
+            # Show the current figure and start a new one
+            plt.title(f"USDA Symbol: {last_symbol}")
+            plt.xlabel('Wavelength')
+            plt.ylabel('Reflectance')
+            #reduce the number of ticks on the x axis
+            plt.xticks(np.arange(0, len(xpoints), len(xpoints)/10), fontsize=8)
+            plt.show()
+            plt.figure()
+            #save plot as a png file
+            plt.savefig(f'./data/{last_symbol}.png')
+
+        # Plot the data
+        plt.plot(xpoints, ypoints, label=f"Sample {index}")
+        last_symbol = symbol
+
+    # Show the last figure
+    if last_symbol is not None:
+        plt.title(f"USDA Symbol: {last_symbol}")
+        plt.xlabel('Wavelength')
+        plt.ylabel('Reflectance')
+        plt.xticks(np.arange(0, len(xpoints), len(xpoints)/10), fontsize=8) 
+        plt.show()
+        plt.savefig(f'./data/{last_symbol}.png') 
+
+    for row in data.iterrows():
+        # Ensure xpoints and ypoints are 1D arrays for plotting
+        ypoints = data.iloc[row, 11:].values 
+        #convert from np.float to float
+        ypoints = np.array(ypoints, dtype=float)
+        #print(xpoints)
+        # Plot the data
+    
+    plt.plot(xpoints, ypoints)  
+
+  
+    #print(ypoints)
+
+    
+    #reduce the number of x ticks to 10
+    plt.xticks(np.arange(0, len(xpoints), len(xpoints)/10), fontsize=8)
+
+    #set the x and y axis labels    
+    plt.xlabel('Wavelength', fontsize=10)
+    plt.ylabel('Reflectance', fontsize=10)
+    plt.show() """
+
+if __name__ == "__main__":
+    main()
