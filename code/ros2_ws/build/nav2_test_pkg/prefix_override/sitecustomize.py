56a30662652e28846c2eabd65b1d04811b9657fe
Ro_gdre5438_bot <roguenobody247@gmail.com>
2025-05-08 16:11:01 +1000

    removed uneeded files

--- code/ros2_ws/build/nav2_test_pkg/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/rogue/codeSpace/repos/LinuxCodeBackUp/code/ros2_ws/install/nav2_test_pkg'
