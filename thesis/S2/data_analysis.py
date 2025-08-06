aa13e187372b3c9aa1ed461371d7a87d84f5d299
Ro_gdre5438_bot <roguenobody247@gmail.com>
2025-08-06 12:05:13 +1000

    TODOs

---
+++ thesis/S2/data_analysis.py
@@ -0,0 +1,7 @@
+#TODO: Extract data from the csv's in the data folder. Stort by Conditions and Genotype. 
+#CSV files: data/maize_2018_2019_unl_metadata.csv, data/maize_2018_2019_unl_spectra.csv, data/maize_2018_2019_unl_traits.csv
+
+#TODO: perform wavelength reflectance plot, PCA, correlation analysis
+
+#TODO: sort into test and training data sets 40% - 60% split. Keep consistance for mulit differnt combinations of x and y data. 
+    #   where Y data is always the given VI found in data/maize_2018_2019_unl_traits.csv
